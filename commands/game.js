bb4b6881122c63041c9617f62d93ec7a1e0746ac
R. A. Souza <alves.wm@gmail.com>
2021-07-09 20:43:19 -0300

    refactors game command

--- commands/game.js
+++ commands/game.js
@@ -1,45 +1,49 @@
 const { MessageEmbed } = require('discord.js')
 const size = require('filesize')
 const Gamepass = require('../services/GamePass')
+const Logger = require('../logger')
+
+const buildMsg = game => {
+  const description = game.ProductDescription.length > 300 ? `${game.ProductDescription.slice(0, 300)}...` : game.ProductDescription
+  const preco = (game.ApproximateSizeInBytes / (1024*1024*1024)) / Number(game.Price.MSRP.slice(1))
+
+  return new MessageEmbed({
+    title: game.ProductTitle,
+    author: {
+      name: game.DeveloperName
+    },
+    description,
+    image: {
+      url: game.ImageHero.URI
+    },
+    fields: [
+      {
+        name: 'Price',
+        value: game.Price.MSRP,
+        inline: true
+      },
+      {
+        name: 'Size',
+        value: size(game.ApproximateSizeInBytes),
+        inline: true
+      },
+      {
+        name: 'Preço do Kilo do Giga',
+        value: `$${Math.round(preco*100) / 100}/GB`,
+        inline: true
+      }
+    ]
+  })
+}
 
 module.exports = {
   name: 'game',
   description: 'Show a game',
   async execute(message, args) {
     const game = await Gamepass.getById('BQ1W1T1FC14W')
+    const gameMsg = buildMsg(game)
 
-    const description = game.ProductDescription.length > 300 ? `${game.ProductDescription.slice(0, 300)}...` : game.ProductDescription
-    const preco = (game.ApproximateSizeInBytes / (1024*1024*1024)) / Number(game.Price.MSRP.slice(1))
-
-    const gameMsg = new MessageEmbed({
-      title: game.ProductTitle,
-      author: {
-        name: game.DeveloperName
-      },
-      description,
-      image: {
-        url: game.ImageHero.URI
-      },
-      fields: [
-        {
-          name: 'Price',
-          value: game.Price.MSRP,
-          inline: true
-        },
-        {
-          name: 'Size',
-          value: size(game.ApproximateSizeInBytes),
-          inline: true
-        },
-        {
-          name: 'Preço do Kilo do Giga',
-          value: `$${Math.round(preco*100) / 100}/GB`,
-          inline: true
-        }
-      ]
-    })
-
-    console.log(`game.ImageTile.URI`, game.ImageTile.URI)
+    Logger.debug(game)
     message.channel.send(gameMsg)
   }
 }
