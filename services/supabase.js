911bb9990fc30bec45cae92333d03ad1716b7133
R. A. Souza <alves.wm@gmail.com>
2021-07-10 03:49:40 -0300

    refactors sync and db services

--- services/supabase.js
+++ services/supabase.js
@@ -24,6 +24,41 @@ exports.parse = ({
   }
 }
 
+exports.getGameById = async id => {
+  const { data, error } = await supabase.from('games').select('*').eq('id', id)
+
+  if (error) Logger.error(error)
+  if (data.length === 0) return null
+
+  return data[0]
+}
+
+exports.insertGame = async game => {
+  const { data, error } = await supabase.from('games').insert(game)
+  
+  if (error) Logger.error(error)
+  return data
+}
+
+exports.syncGame = async id => {
+  const date = new Date()
+  const { data, error } = await supabase.from('games').update({ last_sync: date }).eq('id', id)
+  
+  Logger.debug(`${data.title} updated at ${date.toISOString()}`)
+  if (error) Logger.error(error)
+
+  return data
+}
+
+exports.cleanupGamesBefore = async date => {
+  const { data, error } = await supabase.from('games').delete().lt('last_sync', date)
+  data.forEach(game => Logger.info(`Cleaning up ${game.title}`))
+
+  if (error) Logger.error(error)
+
+  return data
+}
+
 exports.newSubscription = async (channel, webhook) => {
   const { data, error } = await supabase.from('channels').insert({ id: channel, webhook })
 
--- sync.js
+++ sync.js
@@ -1,34 +1,24 @@
 require('dotenv').config();
 
-const { createClient } = require('@supabase/supabase-js')
-const discord = require('./services/discord')
 const xbox = require('./services/xbox')
 const db = require('./services/supabase')
 const Logger = require('./config/logger')
 const config = require('./config/settings.json')
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
 Logger.info('Sync started! Checking new games...');
 
 const sync = async () => {
   const catalog = await xbox.getCatalog()
   Object.keys(catalog).forEach(async id => {
-    const game = db.parse(catalog[id])
-    let { data, error } = await supabase.from('games').select('*').eq('id', id)
-    if (error) Logger.error(error)
+    const game = db.getGameById(id)
 
-    if (data.length === 0) {
-      Logger.info(`Game ${game.title} not found. Inserting on database...`)
-      let { data, error } = await supabase.from('games').insert(game)
-      if (error) Logger.error(error)
+    if (!game) {
+      const data = db.parse(catalog[id])
+      Logger.info(`Game ${data.title} not found. Inserting on database...`)
+      db.insertGame(data)
     } else {
-      const date = new Date()
-      let { data, error } = await supabase.from('games').update({ last_sync: date }).eq('id', game.id)
-      Logger.debug(`${game.title} updated at ${date}`)
-      if (error) Logger.error(error)
+      db.syncGame(id)
     }
-
-  
   })
 }
 
@@ -37,10 +27,7 @@ const cleanup = async () => {
   const today = new Date()
   today.setUTCHours(0,0,0,0)
 
-  let { data, error } = await supabase.from('games').delete().lt('last_sync', today.toISOString())
-  data.forEach(game => Logger.info(`Cleaning up ${game.title}`))
-
-  if (error) Logger.error(error)
+  db.cleanupGamesBefore(today)
 }
 
 process.on('unhandledRejection', (error,) => {
