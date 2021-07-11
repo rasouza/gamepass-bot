f06a4d4d78207586c8c9c3eeb14b3fcfbed30a8d
R. A. Souza <alves.wm@gmail.com>
2021-07-11 06:05:57 -0300

    encapsulates discord client handlers (solves #6)

---
+++ src/handlers/message.ts
@@ -0,0 +1,30 @@
+import fs from 'fs'
+
+import { Collection, Message } from "discord.js";
+import { Command } from "../interfaces";
+import { prefix } from '../config/settings.json'
+
+const COMMAND_RELATIVE = '../commands'
+const COMMAND_ABSOLUTE = './src/commands'
+
+const commands: Collection<string, Command> = new Collection()
+const commandFiles = fs.readdirSync(COMMAND_ABSOLUTE).filter((file: string) => file.endsWith('.ts'))
+commandFiles.forEach(async file => {
+  const command = (await import(`${COMMAND_RELATIVE}/${file}`)).default
+  commands.set(command.name, command)
+})
+
+export default function messageHandler(message: Message): void {
+  if (!message.content.startsWith(prefix) || message.author.bot) return
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/)
+  const command = args.shift()?.toLowerCase()
+  if (!command || !commands.has(command)) return
+  
+  try {
+    commands.get(command)?.execute(message, args)
+  } catch (error) {
+    console.error(error)
+    message.reply('There was an error trying to execute that command')
+  }
+}
--- src/services/discord.ts
+++ src/services/discord.ts
@@ -1,30 +1,15 @@
-import fs from 'fs'
 import filesize from 'filesize'
-import { Client, Collection, Webhook, MessageEmbed } from 'discord.js'
+import { Client, Webhook, MessageEmbed } from 'discord.js'
 
 import { username, avatarURL } from '../config/settings.json'
-import { Command, Embed } from '../interfaces'
+import { Embed } from '../interfaces'
 import Game from '../domain/Game'
 
 const MAX_LENGTH = 300
 
-export function createLogin(token: string): Client {
-  const client = new Client();
-  client.login(token)
-
-  return client
-}
-
-export function loadCommands(path: string): Collection<string, Command> {
-  const commands: Collection<string, Command> = new Collection()
-  const commandFiles = fs.readdirSync(path).filter((file: string) => file.endsWith('.ts'))
-  commandFiles.forEach(async file => {
-    const command = (await import(`../commands/${file}`)).default
-    commands.set(command.name, command)
-  })
-
-  return commands
-}
+const client = new Client();
+client.login(process.env.DISCORD_TOKEN)
+export { client }
 
 export function createEmbed(game: Game): MessageEmbed {
   
@@ -49,8 +34,14 @@ export function createEmbed(game: Game): MessageEmbed {
   return msg
 }
 
-export function broadcast(webhooks: Webhook[], msg: string, embed: MessageEmbed): void {
+export function broadcast(webhooks: Webhook[], msg: string, game: Game): void {
+  const embed = createEmbed(game)
+  
   webhooks.forEach(webhook => {
     webhook.send(msg, { username, avatarURL, embeds: [embed] })
   })
+}
+
+export async function getAllWebhooks(ids: string[]): Promise<Webhook[] | null> {
+  return await Promise.all(ids.map(id => client.fetchWebhook(id))) 
 }
