c2a5ec7c5633a8983d5e5973b5ed95a9f1a4c4a7
R. A. Souza <alves.wm@gmail.com>
2021-07-11 06:05:57 -0300

    encapsulates discord client handlers (solves #6)

--- bot.ts
+++ bot.ts
@@ -1,50 +1,14 @@
 import dotenv from 'dotenv'
-import fs from 'fs'
 dotenv.config()
 
 import Sentry from './src/config/sentry'
-import Logger from './src/config/logger'
-import { prefix, username } from './src/config/settings.json'
-import { createEmbed, broadcast, createLogin, loadCommands } from './src/services/discord'
-import Game from './src/domain/Game'
-import { Collection } from 'discord.js'
-import { Command } from './src/interfaces'
 
-const COMMAND_PATH = './src/commands'
+import { client } from './src/services/discord'
+import messageHandler from './src/handlers/message'
+import readyHandler from './src/handlers/ready'
 
-const client = createLogin(process.env.DISCORD_TOKEN || '')
-const commands = loadCommands(COMMAND_PATH)
-
-
-client.on('ready', async () => {
-  // const subscriptions = await getSubscriptions()
-  // if (!subscriptions) return 
-
-  // const webhooks = await Promise.all(subscriptions.map(sub => client.fetchWebhook(sub.webhook)))
-
-  // onInsert((payload) => {
-  //   const game = new Game(payload.new)
-  //   const gameEmbed = createEmbed(payload.new)
-  //   broadcast(webhooks, 'A new game has arrived!', gameEmbed)
-  // })
-  Logger.info(`${username} is ready!`)
-})
-
-client.on('message', async msg => {
-  if (!msg.content.startsWith(prefix) || msg.author.bot) return
-
-  const args = msg.content.slice(prefix.length).trim().split(/ +/)
-  const command = args.shift()?.toLowerCase()
-  if (!command || !commands.has(command)) return
-  
-  try {
-    commands.get(command)?.execute(msg, args)
-  } catch (error) {
-    console.error(error)
-    msg.reply('There was an error trying to execute that command')
-  }
-})
-
+client.on('ready', readyHandler)
+client.on('message', messageHandler)
 
 process.on('unhandledRejection', (error) => {
   Sentry.captureException(error)
--- src/models/game.ts
+++ src/models/game.ts
@@ -1,10 +1,8 @@
-import { SupabaseRealtimePayload } from '@supabase/supabase-js'
 import Logger from '../config/logger'
 
 import Game from '../domain/Game'
 import { GameModel } from '../interfaces'
 import { DB } from './base'
-
 
 export default class GameDB extends DB<GameModel, Game> {
   constructor() {
@@ -20,42 +18,12 @@ export default class GameDB extends DB<GameModel, Game> {
     return data
   }
 
-  onInsert(fnInsert: (payload: SupabaseRealtimePayload<GameModel>) => void): void {
+  onInsert(fnInsert: (game: Game) => void): void {
     this.table.on('INSERT', payload => {
       Logger.debug('New game inserted', { game: payload.new })
-      fnInsert(payload)
+      const game = new Game(payload.new)
+
+      fnInsert(game)
     }).subscribe()
   }
 }
-
-// export async function syncGame(id: string): Promise<GameModel | null> {
-//   const date = new Date()
-//   const { data, error } = await supabase.from<GameModel>('games').update({ last_sync: date }).eq('id', id).single()
-
-//   Logger.debug(`${data?.title} updated at ${date.toISOString()}`)
-//   if (error) Logger.error(error)
-
-//   return data
-// }
-
-// export function parse ({ 
-//   ProductTitle: title, 
-//   DeveloperName: developer, 
-//   ImageHero: image, 
-//   Price: price,
-//   ApproximateSizeInBytes: size,
-//   ProductDescription: description,
-//   StoreId: id
-// }: XboxGame): Game {
-//   const priceInt = price?.MSRP ? Math.round(Number(price.MSRP.slice(1))*100) : null
-
-//   return {
-//     id,
-//     title, 
-//     developer, 
-//     image: image?.URI, 
-//     price: priceInt, 
-//     size, 
-//     description
-//   }
-// }
