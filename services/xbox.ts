42c2e62c4979b49874a5c48f5c3ae5a848b77549
R. A. Souza <alves.wm@gmail.com>
2021-07-10 23:24:16 -0300

    refactors all javascript

--- services/xbox.ts
+++ services/xbox.ts
@@ -1,12 +1,16 @@
-const axios = require('axios').default
-const Logger = require('../config/logger')
-
+import axios from 'axios'
+import Logger from '../config/logger'
 
 const client = axios.create({
   baseURL: 'https://catalog.gamepass.com'
 })
 
-const getByIds = async ids => {
+interface Sigls {
+  id: string
+}
+
+// TODO: return a list of products
+async function getByIds(ids: string[]) {
   const params = {
     market: 'US',
     language: 'en-us',
@@ -23,14 +27,14 @@ const getByIds = async ids => {
   return resp.data.Products
 }
 
-exports.getCatalog = async () => {
+export async function getCatalog() {
   const params = {
     id: 'fdd9e2a7-0fee-49f6-ad69-4354098401ff',
     language: 'en-us',
     market: 'US'
   }
   const resp = await client.get('/sigls/v2', { params })
-  const ids = resp.data.slice(1).map(game => game.id)
+  const ids = resp.data.slice(1).map((game: Sigls) => game.id)
   Logger.debug(`Found ${ids.length} games`)
 
   const games = getByIds(ids)
