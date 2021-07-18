8ca458cb313b0340e2f5cd6043a2ef40b4d6e571
Rodrigo Souza <alves.wm@gmail.com>
2021-07-18 01:10:32 -0300

    Add Immutable data structures (#11)
    * perf(sync): adds immutable.js
    
    * fix(db): return empty list when none got
    
    * ci: add Dockerfile
    
    * build(docker): remove Dockerfile
    
    [skip ci]

--- sync.ts
+++ sync.ts
@@ -1,4 +1,5 @@
 
+import { Set } from 'immutable'
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -10,37 +11,24 @@ import { startTransaction } from './src/config/sentry'
 
 const gameDB = new GameDB()
 
-async function getInsertDiff (ids: string[]): Promise<string[]> {
-  return await ids.reduce(async (acc: Promise<string[]>, id: string) => {
-    const exists = await gameDB.exists(id)
-    if (!exists) return [...(await acc), id]
-
-    return acc
-  }, Promise.resolve([]))
-}
-
-async function getCleanupDiff (ids: string[]): Promise<string[]> {
-  const games = await gameDB.getAll()
-
-  return games?.map((game) => game.id).filter((id) => !ids.includes(id)) || []
-}
-
 async function sync () {
   Logger.info('Sync started! Checking new games...')
   const transaction = startTransaction('XBox Sync')
 
-  const XboxCatalog = await getIdCatalog()
-  const insertDiff = await getInsertDiff(XboxCatalog)
-  const cleanupDiff = await getCleanupDiff(XboxCatalog)
+  const XboxCatalog = Set(await getIdCatalog())
+  const games = Set((await gameDB.getAll()).map((game) => game.id))
 
-  if (insertDiff.length > 0) {
-    const newGames = await searchGames(insertDiff)
+  const insertDiff = XboxCatalog.subtract(games)
+  const cleanupDiff = games.subtract(XboxCatalog)
+
+  if (insertDiff.size > 0) {
+    const newGames = await searchGames(insertDiff.toArray())
     gameDB.insert(newGames)
     Logger.info('New games inserted', { newGames })
   }
 
-  if (cleanupDiff.length > 0) {
-    cleanupDiff.forEach(async (id) => {
+  if (cleanupDiff.size > 0) {
+    cleanupDiff.forEach(async id => {
       const game = await gameDB.delete(id)
       Logger.info(`${game?.title} is being deleted`, { game })
     })
