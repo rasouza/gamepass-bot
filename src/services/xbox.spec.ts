b21bdec2e3c90581272745e2320479b28ef4c858
R. A. Souza <alves.wm@gmail.com>
2021-08-07 03:27:06 -0300

    test(jest): add xbox service test

--- src/services/xbox.spec.ts
+++ src/services/xbox.spec.ts
@@ -1,9 +1,63 @@
-jest.mock('axios')
+import path from 'path'
+import { back as nockBack } from 'nock'
+import { getIdCatalog, searchGames } from './xbox'
+import Game from 'domain/Game'
+
+nockBack.fixtures = path.join(__dirname, '__fixtures__')
+nockBack.setMode('record')
 
 describe('services/xbox.ts', () => {
   describe('getIdCatalog()', () => {
-    it('returns a list of games by ID', () => {
+    let catalog
+    beforeEach(async () => {
+      const { nockDone } = await nockBack('catalog.json')
+      catalog = await getIdCatalog()
+      nockDone()
+    })
 
+    it('returns a list of games by ID', async () => {
+      expect(catalog.length).toBeGreaterThan(200)
+    })
+
+    it('returns a valid ID', () => {
+      expect(catalog[0].length).toBe(12)
+    })
+  })
+
+  describe('searchGames()', () => {
+    let games: Game[]
+
+    beforeAll(async () => {
+      const { nockDone } = await nockBack('search.json')
+      await searchGames(['9ND0CG3LM22K', '9NJWTJSVGVLJ'])
+      await searchGames(['test'])
+      nockDone()
+    })
+
+    beforeEach(async () => {
+      nockBack('search.json')
+      games = await searchGames(['9ND0CG3LM22K', '9NJWTJSVGVLJ'])
+    })
+
+    describe('given valid IDs', () => {
+      it('returns a list of games', () => {
+        expect(games).toHaveLength(2)
+      })
+
+      it('should contain a game', () => {
+        const game = games.shift()
+        expect(game).toMatchObject({
+          title: 'A Plague Tale: Innocence - Windows 10',
+          developer: 'Asobo Studio'
+        })
+      })
+    })
+
+    describe('given an invalid ID', () => {
+      it('should return empty', async () => {
+        const games = await searchGames(['test'])
+        expect(games).toHaveLength(0)
+      })
     })
   })
 })
