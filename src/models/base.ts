05b4f89a61e7410f1074b9769a1832788af24657
R. A. Souza <alves.wm@gmail.com>
2021-08-07 21:30:33 -0300

    style(eslint): use prettier with standard

--- src/models/base.ts
+++ src/models/base.ts
@@ -1,7 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 import Logger from '@/config/logger'
 
-export const client = createClient(process.env.SUPABASE_URL as string, process.env.SUPABASE_KEY as string)
+export const client = createClient(
+  process.env.SUPABASE_URL as string,
+  process.env.SUPABASE_KEY as string
+)
 
 interface PK {
   id: string | number
@@ -11,54 +14,63 @@ export abstract class DB<Model extends PK, Domain> {
   abstract name: string
   client = client
 
-  getTable () {
+  getTable() {
     return this.client.from<Model>(this.name)
   }
 
-  async getAll (): Promise<Model[]> {
+  async getAll(): Promise<Model[]> {
     const { data, error } = await this.getTable().select('*')
     if (error) Logger.error(error)
 
     return data ?? []
   }
 
-  async getAllById (ids: Model[keyof Model][]): Promise<Model[]> {
+  async getAllById(ids: Model[keyof Model][]): Promise<Model[]> {
     const { data, error } = await this.getTable().select('*').in('id', ids)
     if (error) Logger.error(error)
 
     return data ?? []
   }
 
-  async getById (id: Model[keyof Model]): Promise<Model | null> {
-    const { data, error } = await this.getTable().select('*').eq('id', id).maybeSingle()
+  async getById(id: Model[keyof Model]): Promise<Model | null> {
+    const { data, error } = await this.getTable()
+      .select('*')
+      .eq('id', id)
+      .maybeSingle()
     if (error) Logger.error(error)
 
     return data
   }
 
-  async insert (domain: Domain | Domain[]): Promise<Model | Model[] | null> {
+  async insert(domain: Domain | Domain[]): Promise<Model | Model[] | null> {
     const { data, error } = await this.getTable().insert(domain)
 
     if (error) Logger.error(error)
     return data
   }
 
-  async update (id: Model[keyof Model], model: Model): Promise<Model | null> {
-    const { data, error } = await this.getTable().update(model).eq('id', id).single()
+  async update(id: Model[keyof Model], model: Model): Promise<Model | null> {
+    const { data, error } = await this.getTable()
+      .update(model)
+      .eq('id', id)
+      .single()
     if (error) Logger.error(error)
 
     return data
   }
 
-  async delete (id: Model[keyof Model]): Promise<Model | null> {
+  async delete(id: Model[keyof Model]): Promise<Model | null> {
     const { data, error } = await this.getTable().delete().eq('id', id).single()
     if (error) Logger.error(error)
 
     return data
   }
 
-  async exists (id: Model[keyof Model]): Promise<boolean> {
-    const { data, error } = await this.getTable().select('*').eq('id', id).maybeSingle()
+  async exists(id: Model[keyof Model]): Promise<boolean> {
+    const { data, error } = await this.getTable()
+      .select('*')
+      .eq('id', id)
+      .maybeSingle()
     if (error) Logger.error(error)
 
     return !!data
