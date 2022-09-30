6a3442cb33944224a7fdcb50cd1b796872cadcd0
R. A. Souza <alves.wm@gmail.com>
2022-09-30 18:00:47 +0200

    fix: :bug: fix Game interface

--- src/domain/Game.ts
+++ src/domain/Game.ts
@@ -2,9 +2,8 @@ export default interface Game {
   id: string
   title: string
   developer: string
-  image: string | null
-  price: number | null
-  size: number | null
   description: string
-  last_sync?: Date // eslint-disable-line camelcase
+  image?: string
+  price?: number
+  size?: number
 }
